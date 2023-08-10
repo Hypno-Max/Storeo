95806ef9d756ae7adb5c0bfd4f0f34c1ac27dcc7
Maxime P <115455857+Hypno-Max@users.noreply.github.com>
2023-08-10 11:43:13 +0200

    Add files via upload

---
+++ historique.php
@@ -0,0 +1,66 @@
+<?php 
+/* Template Name: historique */
+get_header(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>accueil</title>
+</head>
+<body>
+<div class="container-command">
+        <h1>Historique des Commandes</h1>
+        <ul class="command-list">
+            <li>
+                <span class="date">23 Avril 2023</span>
+                <span class="order">Dragon's lair</span>
+                <span class="amount">Nes</span>
+            </li>
+            <li>
+                <span class="date">28 Avril 2023</span>
+                <span class="order">Prince of Persia</span>
+                <span class="amount">Ps2</span>
+            </li>
+            <li>
+                <span class="date">12 Mai 2023</span>
+                <span class="order">Ratchet et clank</span>
+                <span class="amount">PS2</span>
+            </li>
+            <li>
+                <span class="date">28 Mai 2023</span>
+                <span class="order">Borderlands 2</span>
+                <span class="amount">PS3</span>
+            </li>
+            <li>
+                <span class="date">28 Mai 2023</span>
+                <span class="order">Borderlands 3</span>
+                <span class="amount">PS4</span>
+            </li>
+            <li>
+                <span class="date">12 Juin 2023</span>
+                <span class="order">Spiderman : le règne des ombres</span>
+                <span class="amount">PS2</span>
+            </li>
+            <li>
+                <span class="date">14 Juin 2023</span>
+                <span class="order">Fallout 4</span>
+                <span class="amount">PS4</span>
+            </li>
+            <li>
+                <span class="date">18 Juin 2023</span>
+                <span class="order">Ratchet Gladiator</span>
+                <span class="amount">PS2</span>
+            </li>
+            <li>
+                <span class="date">22 Juin 2023</span>
+                <span class="order">Sly cooper : Trilogy</span>
+                <span class="amount">PS3</span>
+            </li>
+            <!-- Ajoutez d'autres éléments de commande ici -->
+        </ul>
+    </div>
+</body>
+<hr>
+</html>
+<?php get_footer(); ?>
+
+
